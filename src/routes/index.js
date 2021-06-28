0f524215a9c97c196e80503aaedcee0801cc57c7
Posada-rgb <72574472+Posada-rgb@users.noreply.github.com>
2021-06-28 02:53:13 -0500

    Add files via upload

---
+++ src/routes/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const model = require('../model/valores')();
+
+const Valor = require('../model/valores');
+
+
+router.get('/', async (req, res)=> {
+    const valores = await Valor.find();
+    console.log(valores)
+    /**res.json(valores);*/
+    res.render('index.ejs', {
+        valores
+    });
+});
+
+router.post('/add', async (req, res)=> {
+    const valor = new Valor(req.body);
+    await valor.save();
+    res.redirect('/');
+    /**res.json(req.body);*/
+});
+
+module.exports = router;
